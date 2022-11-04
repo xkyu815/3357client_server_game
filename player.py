be06175b1bcfc64cd994d144131ca9352b79d8e3
Xiaoke Yu <xkyu815@gmail.com>
2022-11-04 02:25:19 -0400

    when another player enters the room a player is in, the server notifies them of the new player.

--- player.py
+++ player.py
@@ -1,55 +1,198 @@
-from re import U
 import socket
-import argparse
-from urllib.parse import urlparse
 import signal
 import sys
+import argparse
+from urllib.parse import urlparse
+import selectors
 
-bufferSize = 1024
+# Selector for helping us select incoming data from the server and messages typed in by the user.
 
-parser = argparse.ArgumentParser("client parameters passed")
-parser.add_argument('playerName',type=str,help = "name of player")
-parser.add_argument('serverAddress',type=str,help = "address of server")
-options = parser.parse_args()
-parse_result = urlparse(options.serverAddress)
-playerName = options.playerName
-hostname = parse_result.hostname
-port = parse_result.port
-if (playerName == None or hostname == None or port == None):
-	mesg = "Invalid server address\n"
-	sys.stdout.write(mesg)
-	quit()
+sel = selectors.DefaultSelector()
 
-# Create a UDP socket at client side
+# Socket for sending messages.
 
-UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#Send to server using created UDP socket
-def join():
-	msgFromClient       = "Room will wait for players at port: {}\nUser {} joined from address".format(port,playerName)
-	bytesToSend         = str.encode(msgFromClient)
-	UDPClientSocket.sendto(bytesToSend,(hostname,port))
+# Server address.
 
-	msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-	address = msgFromServer[1]
-	pw_bytes = msgFromServer[0].decode("utf-8")
-	sys.stdout.write(pw_bytes + '\n')
+server = ('', '')
 
-def exit(sig, frame):
-	sys.exit(0)
+# User name for player.
 
-if __name__ == "__main__":
-	signal.signal(signal.SIGINT, exit)
-	join()
-# Pass user input to the server
-	while(True):
-		userinput = input('> ')
-		if userinput == 'exit':
-			quit()
-		commandToSend = str.encode(userinput)
-		UDPClientSocket.sendto(commandToSend,(hostname,port))
-		resultFromServer = UDPClientSocket.recvfrom(bufferSize)
-		result = resultFromServer[0].decode("utf-8")
-		sys.stdout.write(result + '\n') 
+name = ''
 
-	
+# Inventory of items.
+
+inventory = []
+
+# Signal handler for graceful exiting.  Let the server know when we're gone.
+
+def signal_handler(sig, frame):
+    print('Interrupt received, shutting down ...')
+    message='exit'
+    client_socket.send(message.encode())
+    for item in inventory:
+        message = f'drop {item}'
+        client_socket.send(message.encode())
+    sys.exit(0)
+
+# Simple function for setting up a prompt for the user.
+
+def do_prompt(skip_line=False):
+    if (skip_line):
+        print("")
+    print("> ", end='', flush=True)
+
+# Function to handle incoming messages from server.
+def handle_keyboard_input(file, mask):
+    # Prompt the user before beginning.
+
+        # Get a line of input.
+
+        line=sys.stdin.readline()[:-1]
+        client_socket.send(line.encode())
+
+        # Process command and send to the server.
+        do_prompt()
+        process_command(client_socket,mask)
+
+# Function to join a room.
+
+def join_room():
+    message = f'join {name}'
+    client_socket.send(message.encode())
+    response = client_socket.recv(1024)
+   # client_socket.setblocking(False)
+    print(response.decode())
+
+def send_player_name(sock,mask):
+    playername = name
+    sock.send(playername.encode())
+    playerNotifyfromServer = sock.recv(1024).decode()
+    print(playerNotifyfromServer)
+
+# Function to handle commands from the user, checking them over and sending to the server as needed.
+
+def process_command(file,mask):
+    # Parse command.
+    command = file.recv(1024).decode()
+    words = command.split()
+
+    # Check if we are dropping something.  Only let server know if it is in our inventory.
+
+    if (words[0] == 'drop'):
+        if (len(words) != 2):
+            print("Invalid command")
+            return
+        elif (words[1] not in inventory):
+            print(f'You are not holding {words[1]}')
+            return
+
+    # Send command to server, if it isn't a local only one.
+
+    if (command != 'inventory'):
+        message = f'{command}'
+        client_socket.send(message.encode())
+
+    # Check for particular commands of interest from the user.
+
+    if (command == 'exit'):
+        for item in inventory:
+            message = f'drop {item}'
+            client_socket.send(message.encode())
+        sys.exit(0)
+    elif (command == 'look'):
+        response = client_socket.recv(1024)
+        print(response.decode())
+    elif (command == 'inventory'):
+        print("You are holding:")
+        if (len(inventory) == 0):
+            print('  No items')
+        else:
+            for item in inventory:
+                print(f'  {item}')
+    elif (words[0] == 'take'):
+        response = client_socket.recv(1024)
+        print(response.decode())
+        words = response.decode().split()
+        if ((len(words) == 2) and (words[1] == 'taken')):
+            inventory.append(words[0])
+    elif (words[0] == 'drop'):
+        response = client_socket.recv(1024)
+        print(response.decode())
+        inventory.remove(words[1])
+    else:
+        response = client_socket.recv(1024)
+        print(response.decode())
+    client_socket.setblocking(False)
+
+# Our main function.
+
+def main():
+
+    global name
+    global client_socket
+    global server
+
+    # Register our signal handler for shutting down.
+
+    signal.signal(signal.SIGINT, signal_handler)
+
+    # Check command line arguments to retrieve a URL.
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("name", help="name for the player in the game")
+    parser.add_argument("server", help="URL indicating server location in form of room://host:port")
+    args = parser.parse_args()
+
+    # Check the URL passed in and make sure it's valid.  If so, keep track of
+    # things for later.
+
+    try:
+        server_address = urlparse(args.server)
+        if ((server_address.scheme != 'room') or (server_address.port == None) or (server_address.hostname == None)):
+            raise ValueError
+        host = server_address.hostname
+        port = server_address.port
+        server = (host, port)
+    except ValueError:
+        print('Error:  Invalid server.  Enter a URL of the form:  room://host:port')
+        sys.exit(1)
+    name = args.name
+
+     # Now we try to make a connection to the server.
+
+    print('Connecting to server ...')
+    try:
+        client_socket.connect((host, port))
+    except ConnectionRefusedError:
+        print('Error:  That host or port is not accepting connections.')
+        sys.exit(1)
+
+    # The connection was successful, so we can prep and send a registration message.
+
+    print('Connection to server established. Sending intro message...\n')
+    # Send message to enter the room.
+
+    join_room()
+   
+
+    # Set up our selector.
+
+    client_socket.setblocking(False)
+    sel.register(client_socket, selectors.EVENT_READ,
+                 send_player_name)
+    #sel.register(sys.stdin, selectors.EVENT_READ, handle_keyboard_input)
+
+    
+    # We now loop forever, sending commands to the server and reporting results
+
+    while True:
+        events = sel.select()
+        for key, mask in events:
+            callback = key.data
+            callback(key.fileobj, mask)
+ 
+
+if __name__ == '__main__':
+    main()
--- room.py
+++ room.py
@@ -1,107 +1,231 @@
 import socket
-import argparse
 import signal
 import sys
+import argparse
+import selectors
 
-bufferSize  = 1024
+# Saved information on the room.
 
-parser = argparse.ArgumentParser("server parameters passed")
-parser.add_argument('port',type=int,help = "port to listen")
-parser.add_argument('name',help = "the name of the room running on the server")
-parser.add_argument('description',help = "description of the room")
-parser.add_argument('items',help = "items to be found in the room initially",nargs="*")
+name = ''
+description = ''
+items = []
+connections = []
 
-options = parser.parse_args()
+# Selector for helping us select incoming data and connections from multiple sources.
 
-description = options.description  # player name from server
-name = options.name     # description from server
-items = options.items   
-localPort = options.port 
+sel = selectors.DefaultSelector()
+# List of clients currently in the room.
 
-# Create a datagram socket
+client_list = []
 
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+# Signal handler for graceful exiting.  
 
- # Bind to address and ip
+def signal_handler(sig, frame):
+    print('Interrupt received, shutting down ...')
+    sys.exit(0)
 
-UDPServerSocket.bind(('', localPort))
+# Search the client list for a particular player.
 
-# initialize inventory
-user_inventory = []
+def client_search(player):
+    for reg in client_list:
+        if reg[0] == player:
+            return reg[1]
+    return None
 
-# Items in the room initially
-itemInRoom = items
 
-def look():
-	if len(itemInRoom)>0 :
-		mesg = "\n{}\n\n{}\n\nIn this room, there are:\n{}".format(name,description,"\n".join(itemInRoom))
-	else:
-		mesg = "\n{}\n\n{}\n\nThe room is empty.".format(name,description)
-	return mesg
+    
+# Search the client list for a particular player by their address.
 
-def take(item):
-	if item not in itemInRoom:
-		mesg = "{} cannot be taken.The item must exist in the room to be taken".format(item)
-	else:
-		mesg = "{} taken".format(item)
-		user_inventory.append(item)
-		itemInRoom.remove(item)
-	return mesg
+def client_search_by_address(address):
+    for reg in client_list:
+        if reg[1] == address:
+            return reg[0]
+    return None
 
-def inventory():
-	if user_inventory == []:
-		mesg = "Holding nothing"
-	else:
-		mesg = "You are holding: \n{}".format("\n".join(user_inventory))
-	return mesg
+# Add a player to the client list.
 
-def drop(item):
-	if item not in user_inventory:
-		mesg = "You are not holding {}".format(item)
-	else:
-		user_inventory.remove(item)
-		itemInRoom.append(item)
-		mesg = "You are dropping {}".format(item)
-	return mesg
+def client_add(player, address):
+    registration = (player, address)
+    client_list.append(registration)
 
-def exit(sig, frame):
-	while user_inventory != []:
-		drop(user_inventory[0])
-	sys.exit("Interrupt received, shutting down...")
+# Remove a client when disconnected.
 
-def join():
-	mesg_description = "Room Starting Description:\n{}\n\n{}\n\nIn this room, there are:\n{}".format(name,description,"\n".join(items))
-	sys.stdout.write(mesg_description + '\n')
-	bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)  # Receiving a request from client
-	bytesToSend = str.encode(mesg_description)
-	message = bytesAddressPair[0]
-	address = bytesAddressPair[1]
-	pw_bytes = message.decode("utf-8")
-	print(pw_bytes,address)
-	UDPServerSocket.sendto(bytesToSend,address)  # Sending a reply to client
-	return address
+def client_remove(player):
+    for reg in client_list:
+        if reg[0] == player:
+            client_list.remove(reg)
+            break
 
-if __name__ == "__main__":
-# pass client ip address
-	clientAddress = join()
-	while(True):
-		signal.signal(signal.SIGINT, exit)
-		bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-		message = bytesAddressPair[0]
-		pw_bytes = message.decode("utf-8")
-		tokens = pw_bytes.split(' ')
-		command = tokens[0]
-		if len(tokens) > 1:
-			args = tokens[1]
-		if command == 'look':
-			result = look()
-		elif command == 'take':
-			result = take(args)
-		elif command == 'drop':
-			result = drop(args)
-		elif command == 'inventory':
-			result = inventory()
-		else:
-			result = "Command doesn't exist."
-		bytesToSend = str.encode(result)
-		UDPServerSocket.sendto(bytesToSend,clientAddress)
+# Summarize the room into text.
+
+def summarize_room():
+
+    global name
+    global description
+    global items
+
+    # Pack description into a string and return it.
+
+    summary = name + '\n\n' + description + '\n\n'
+    if len(items) == 0:
+        summary += "The room is empty.\n"
+    elif len(items) == 1:
+        summary += "In this room, there is:\n"
+        summary += f'  {items[0]}\n'
+    else:
+        summary += "In this room, there are:\n"
+        for item in items:
+            summary += f'  {item}\n'
+
+    return summary
+
+# Print a room's description.
+
+def print_room_summary():
+
+    print(summarize_room()[:-1])
+
+# Process incoming message.
+
+def process_message(sock,mask):
+
+    # Parse the message.
+    conn, addr = sock.accept()
+    message = conn.recv(1024).decode()
+    words = message.split()
+
+    # If player is joining the server, add them to the list of players.
+
+    if (words[0] == 'join'):
+        if (len(words) == 2):
+            client_add(words[1],addr)
+            print(f'User {words[1]} joined from address {addr}')
+            return summarize_room()[:-1]
+        else:
+            return "Invalid command"
+
+    # If player is leaving the server. remove them from the list of players.
+
+    elif (message == 'exit'):
+        client_remove(client_search_by_address(addr))
+        return 'Goodbye'
+
+    # If player looks around, give them the room summary.
+
+    elif (message == 'look'):
+        return summarize_room()[:-1]
+            
+    # If player takes an item, make sure it is here and give it to the player.
+
+    elif (words[0] == 'take'):
+        if (len(words) == 2):
+            if (words[1] in items):
+                items.remove(words[1])
+                return f'{words[1]} taken'
+            else:
+                return f'{words[1]} cannot be taken in this room'
+        else:
+            return "Invalid command"
+
+    # If player drops an item, put in in the list of things here.
+
+    elif (words[0] == 'drop'):
+        if (len(words) == 2):
+            items.append(words[1])
+            return f'{words[1]} dropped'
+        else:
+            return "Invalid command"
+
+    # Otherwise, the command is bad.
+
+    else:
+        return "Invalid command"
+
+
+def accept_client(sock,mask):
+    player_sock, addr = sock.accept()
+    welcome = summarize_room().encode()
+    player_sock.send(welcome)
+    #playername = conn.recv(1024).decode()
+    playername = player_sock.recv(1024).decode()
+    client_add(player_sock,playername)
+    for playersock, player_name in client_list:
+        if player_name != playername:
+            message = "\n{} enters the room.".format(playername)
+            playersock.send(message.encode())
+
+def accept_playername(sock,mask):
+    conn, addr = sock.accept()
+    playername = conn.recv(1024).decode()
+    client_add(playername,addr)
+
+    #notifyOtherPlayer = '{} enters the room.'.format(playername)
+    #conn.send(notifyOtherPlayer.encode())
+    #conn.setblocking(False)
+    #sel.register(conn, selectors.EVENT_READ,process_message)
+
+# Our main function.
+
+def main():
+
+    global name
+    global description
+    global items
+    global connections
+
+    # Register our signal handler for shutting down.
+
+    signal.signal(signal.SIGINT, signal_handler)
+
+    # Check command line arguments for room settings.
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("port", type=int, help="port number to list on")
+    parser.add_argument("name", help="name of the room")
+    parser.add_argument("description", help="description of the room")
+    parser.add_argument("item", nargs='*', help="items found in the room by default")
+    args = parser.parse_args()
+
+    port=args.port
+    name = args.name
+    description = args.description
+    items = args.item
+
+    # Report initial room state.
+    print('Room Starting Description:\n')
+    print_room_summary()
+
+    # Create the socket.  We will ask this to work on any interface and to use
+    # the port given at the command line.  We'll print this out for clients to use.
+
+    room_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    room_socket.bind(('', port))
+    print('\nRoom will wait for players at port: ' + str(room_socket.getsockname()[1]))
+    room_socket.listen(100)
+    room_socket.setblocking(False)
+    print('Waiting for incoming client connections ...')
+    sel.register(room_socket,selectors.EVENT_READ, accept_client)
+    
+     # Loop forever waiting for messages from clients.
+
+    while True:
+        events = sel.select()
+        for key,mask in events:
+            callback = key.data
+            callback(key.fileobj,mask)
+        
+        # Receive a packet from a client and process it.
+       # conn, addr = room_socket.accept()
+
+        # Process the message and retrieve a response.
+       # response = process_message(room_socket,mask)
+
+        # Send the response message back to the client.
+
+       # conn.send(response.encode())
+
+
+
+if __name__ == '__main__':
+    main()
+
